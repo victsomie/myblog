8e8fd769ea90f73316199d934c0fd04089156d27
victsomie <victsomie@yahoo.com>
2016-05-11 21:42:10 +0300

    added other routes

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+@app.route('/about')
+def about():
+    return '<h1>This is about</h1>'
+@app.route('/last')
+def last():
+    return '<h1>Finally last page</h1>'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
