3464efadc77503bc31eb524b201a68feaae75aab
victsomie <victsomie@yahoo.com>
2016-05-14 14:49:53 +0300

    added content for database use in view functions

--- app/views.py
+++ app/views.py
@@ -2,10 +2,41 @@
 from flask import render_template, session, redirect, request, escape, url_for
 from app import app
 
-@app.route('/') #[Route decorator] This registers the function as a route
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user =User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+return render_template('index.html',
+                       form = form, name = session.get('name'),
+                       known = session.get('known', False))
+
+
+
+
+
+
+
+
+
+
+@app.route('/home') #[Route decorator] This registers the function as a route
 @app.route('/home') #[Route decorator] This registers the function as a route
 def home():
     return '<h1>Hello World!</h1>'
+
 
 @app.route('/about')
 def about():
