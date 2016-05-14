7e3ccdd057e8eb39cc777e752c7ebfe16ea921ff
victsomie <victsomie@yahoo.com>
2016-05-14 15:00:33 +0300

    added db_create file and content

--- app/views.py
+++ app/views.py
@@ -19,7 +19,7 @@ def index():
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-return render_template('index.html',
+    return render_template('index.html',
                        form = form, name = session.get('name'),
                        known = session.get('known', False))
 
